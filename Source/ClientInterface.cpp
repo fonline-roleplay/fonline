81dd12f41cbffea88d8777af40a1768a5f6db4a9
APAMk2 <APAmk2@users.noreply.github.com>
2025-03-04 17:51:47 +0400

    Fixed wrong `ST_APREGEN` displaying in client

--- Source/ClientInterface.cpp
+++ Source/ClientInterface.cpp
@@ -7460,7 +7460,7 @@ void FOClient::ChaDraw( bool is_reg )
             str = Str::FormatBuf( "%d%%", val );
             break;
 		case ST_APREGEN:
-			str = Str::FormatBuf("%f", float(val) / 100);
+			str = Str::FormatBuf("%d.%d", int(floor(val / 100.0f)), val % 100 );
 			break;
         default:
             str = Str::FormatBuf( "%d", val );
