ff86d687793db33660b3e76c9b9e6d32c4247d96
APAMk2 <APAmk2@users.noreply.github.com>
2025-05-20 18:41:42 +0400

    Kick player critter to global if can't land on hex

--- Source/ServerClient.cpp
+++ Source/ServerClient.cpp
@@ -2329,9 +2329,13 @@ void FOServer::Process_LogIn( ClientPtr& cl )
         {
             WriteLogF( _FUNC_, " - Error add critter to map, client<%s>.\n", cl->GetInfo() );
             cl->Send_TextMsg( cl, STR_NET_HEXES_BUSY, SAY_NETMSG, TEXTMSG_GAME );
-            cl->Disconnect();
-            cl->Data.Id = 0;
-            cl->SetMaps( 0, 0 );
+			if (!MapMngr.TransitToGlobal(cl, 0, FOLLOW_FORCE, true))
+			{
+				WriteLogF(_FUNC_, " - Can't kick client<%s> to global.\n", cl->GetInfo());
+				cl->Disconnect();
+				cl->Data.Id = 0;
+				cl->SetMaps( 0, 0 );
+			}
             return;
         }
 
